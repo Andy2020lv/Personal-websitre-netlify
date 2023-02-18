32749ddbaf139b3e60286199824312ddfeb97b1c
Andy2020lv <andyhernandez5102@gmail.com>
2023-02-18 17:02:39 -0400

    Added nextJS and TS to the frontEnd skill list

--- src/components/SkillsFrontEnd.js
+++ src/components/SkillsFrontEnd.js
@@ -1,6 +1,8 @@
 import React from "react";
 import js from "../images/js.png";
+import ts from "../images/TS.png";
 import react from "../images/react.png";
+import next from "../images/nextJS.png";
 import html5 from "../images/html5.png";
 import css from "../images/css.png";
 
@@ -12,8 +14,16 @@ export default function SkillsFrontEnd() {
         <p>Javascript</p>
       </div>
       <div className="skill">
+        <img width={45} height={25} alt="skillsFrontEnd" src={ts}></img>
+        <p>Typescript</p>
+      </div>
+      <div className="skill">
         <img width={45} height={25} alt="skillsFrontEnd" src={react}></img>
         <p>React</p>
+      </div>
+      <div className="skill">
+        <img width={45} height={25} alt="skillsFrontEnd" src={next}></img>
+        <p>NextJS</p>
       </div>
       <div className="skill">
         <img width={45} height={25} alt="skillsFrontEnd" src={html5}></img>
